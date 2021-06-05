44b3247d246bfa8ba0290b4e58db60c6702af4ce
Marcus.Sousa <marvinsousa83@gmail.com>
2021-06-05 18:41:21 -0300

    Initial Commit + ESLint + Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    trailingComma: 'none',
+    tabWidth: 4,
+    tabs: false,
+    semi: false
+}
